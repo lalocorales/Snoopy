933b3df441cf29cfd8d3558ac270799dc606f2e5
Lab2-Profesores <Lab2-Profesores@LAB2-PROFESORES>
2025-05-20 07:15:03 -0600

    actualizacion

--- Snoopy/Program.cs
+++ Snoopy/Program.cs
@@ -8,6 +8,7 @@ namespace Snoopy
         [STAThread]
         static void Main()
         {
+            //cambios en github
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
